1c03576eadbccb9906c7ba6860ebb3a4c7cbcefd
ReubenReuben <rubensiya@gmail.com>
2024-02-26 19:10:20 +0200

    first commit

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input("enter your name ")
+age= input("Enter your age")
+location = input("enter location")
+
+print(f"hello {name}, you are {age} years old and your location is {location}")
+
